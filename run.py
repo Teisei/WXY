6f3aba4034838dc7cfe66acbdaf1d9e43963cc68
Teisei <teisei.din@gmail.com>
2024-01-26 18:21:02 +0800

    Update run.py

--- run.py
+++ run.py
@@ -5,6 +5,4 @@ from wxcloudrun import app
 
 # 启动Flask Web服务
 if __name__ == '__main__':
-    # app.config['JSON_AS_ASCII'] = False
-    app.json.ensure_ascii = False
     app.run(host=sys.argv[1], port=sys.argv[2])
