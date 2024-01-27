00bd307fdc095d2f3173f54cbf1051f396d8de52
Teisei <teisei.din@gmail.com>
2024-01-28 00:53:30 +0800

    Update views.py

--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -107,28 +107,34 @@ def send_msg():
     data = json.dumps({'code': 200, 'data': {'text': text}})
     return Response(data, mimetype='application/json')
 
-# # 每12小时执行一次job函数
-# def job():
-#     print('定时任务执行中...')
-#     _sendMsg(content='定时任务')
-# def run_schedule():
-#     while True:
-#         schedule.run_pending()
-#         time.sleep(1)
+# 每12小时执行一次job函数
+def job():
+    for i in range(0, 3):
+        try:
+            return _sendMsg(content='定时任务')
+        except Exception as e:
+            app.logger.info('call _sendMsg error......')
+    return "success"
+def run_schedule():
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 # schedule.every(12).hours.do(job)
-# t = threading.Thread(target=run_schedule)
-# t.start()
-# def _sendMsg(content, openid='o7Fnt6ZwAZFjOukruDoOOgJXUeA8'):
-#     url = 'http://api.weixin.qq.com/cgi-bin/message/custom/send'
-#     headers = {'Content-Type': 'application/json'}
-#     info = {
-#         'touser': openid,
-#         'msgtype': 'text',
-#         'text': {
-#             'content': content
-#         }
-#     }
-#     data = json.dumps(info, ensure_ascii=False).encode('utf-8')
-#     response = requests.post(url, headers=headers, json=data)
-#     app.logger.info('接口返回内容:' + response.text)
-#     return response.text
+schedule.every().day.at("00:58:20").do(job)
+schedule.every().day.at("08:30:20").do(job)
+t = threading.Thread(target=run_schedule)
+t.start()
+def _sendMsg(content, openid='o7Fnt6ZwAZFjOukruDoOOgJXUeA8'):
+    url = 'http://api.weixin.qq.com/cgi-bin/message/custom/send'
+    headers = {'Content-Type': 'application/json'}
+    info = {
+        'touser': openid,
+        'msgtype': 'text',
+        'text': {
+            'content': content
+        }
+    }
+    data = json.dumps(info, ensure_ascii=False).encode('utf-8')
+    response = requests.post(url, headers=headers, json=data)
+    app.logger.info('接口返回内容:' + response.text)
+    return response.text
